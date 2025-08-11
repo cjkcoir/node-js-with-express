ac8c62e001d9bc4efca0decd3ca9beb091fca6e2
cjkcoir <j.chellakarthikeyan@gmail.com>
2025-08-11 06:14:42 +0530

    json response is added in app.get()

--- app.js
+++ app.js
@@ -4,8 +4,12 @@ const app = express();
 
 const PORT = 3000;
 
+// app.get("/", (req, res) => {
+//   res.status(200).send("<h1>Get request success</h1>");
+// });
+
 app.get("/", (req, res) => {
-  res.status(200).send("<h1>Get request success</h1>");
+  res.status(200).json({ message: "Success", status: 200 });
 });
 
 app.listen(PORT, () => {
