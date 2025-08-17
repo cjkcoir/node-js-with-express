d546611580db55aacd9a0dd4edfbb79e98b6db1e
cjkcoir <j.chellakarthikeyan@gmail.com>
2025-08-17 19:20:27 +0530

    As per video 36 getMovieById API is created in app.js

--- app.js
+++ app.js
@@ -21,7 +21,8 @@ app.use((req, res, next) => {
   next();
 });
 
-app.get("/api/v1/movies", (req, res) => {
+//127.0.0.1:3000/api/v1/movies --GET
+http: app.get("/api/v1/movies", (req, res) => {
   // Define a GET route at /api/v1/movies
   res.status(200).json({
     // Send an HTTP 200 (OK) response in JSON format --METHOD CHAINING
@@ -35,6 +36,29 @@ app.get("/api/v1/movies", (req, res) => {
   });
 });
 
+// http://127.0.0.1:3000/api/v1/movies/6--GET
+app.get("/api/v1/movies/:id", (req, res) => {
+  const id = req.params.id * 1;
+  let movie = movies.find((element) => element.id === id);
+
+  if (!movie) {
+    res.status(400).json({
+      status: "Failed",
+      message: `Movie with id = ${id} is not in the Database`,
+    });
+  }
+  res.status(200).json({
+    // Send an HTTP 200 (OK) response in JSON format --METHOD CHAINING
+    message: "Success", // Include a success message
+    requestedAt: req.requestedAt,
+    data: {
+      // Wrap movie data inside a "data" object --ENVELOPING
+      movie: movie, // Send the movies list from the file
+    },
+  });
+});
+
+// http://127.0.0.1:3000/api/v1/movies---POST
 app.post("/api/v1/movies", (req, res) => {
   console.log(req.body);
   const newId = movies[movies.length - 1].id + 1;
@@ -43,6 +67,7 @@ app.post("/api/v1/movies", (req, res) => {
   fs.writeFile("./data/movies.json", JSON.stringify(movies), () => {
     res.status(201).json({
       status: "Success",
+      requestedAt: req.requestedAt,
       message: "Movie created",
       data: {
         movie: newMovie,
