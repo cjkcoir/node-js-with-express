c8f2f2bed38714f922199ad4e8e03361e074a267
cjkcoir <j.chellakarthikeyan@gmail.com>
2025-08-27 10:01:25 +0530

    As per video 64 , mongodb atlas, cluster is formed, ciniflix db created

--- server.js
+++ server.js
@@ -1,8 +1,21 @@
 const express = require("express"); // Import the Express framework
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+dotenv.config({ path: "./config.env" });
 
 const app = require("./app"); // Create an Express application instance
 
-const PORT = 3000; // Define the port number for the server
+const PORT = process.env.PORT || 3000; // Define the port number for the server
+
+// console.log(app.get("env"));
+console.log(process.env);
+
+mongoose
+  .connect(process.env.CONNECTION_STRING, { useNewUrlParser: true })
+  .then((conn) => {
+    // console.log(conn);
+    console.log("DB COnnection Successful");
+  });
 
 app.listen(PORT, () => {
   // Start the server and listen for requests on the specified PORT
