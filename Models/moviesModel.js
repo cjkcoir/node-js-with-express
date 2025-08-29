a9bdf1b85726d51972085c3602464bf1c569c528
cjkcoir <j.chellakarthikeyan@gmail.com>
2025-08-30 05:01:52 +0530

    as per video 74 changed

--- Models/moviesModel.js
+++ Models/moviesModel.js
@@ -5,9 +5,12 @@ const moviesSchema = new mongoose.Schema({
     type: String,
     required: [true, "Name is required"],
     unique: true,
+    trim: true,
   },
   description: {
     type: String,
+    required: [true, "Description is required"],
+    trim: true,
   },
   duration: {
     type: Number,
@@ -15,7 +18,40 @@ const moviesSchema = new mongoose.Schema({
   },
   ratings: {
     type: Number,
-    default: 1.0,
+  },
+  totalRatings: {
+    type: Number,
+  },
+  releaseYear: {
+    type: Number,
+    required: [true, "Release Year is required"],
+  },
+  releaseDate: {
+    type: Date,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  genres: {
+    type: [String],
+    required: [true, "Genres is required"],
+  },
+  directors: {
+    type: [String],
+    required: [true, "Directors is required"],
+  },
+  coverImage: {
+    type: String,
+    required: [true, "Cover image is required"],
+  },
+  actors: {
+    type: [String],
+    required: [true, "Actors is required"],
+  },
+  price: {
+    type: Number,
+    required: [true, "Price is required"],
   },
 });
 
