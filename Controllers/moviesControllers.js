545bdcafb2b4452d06d036a9bfa1799cab07acf3
cjkcoir <j.chellakarthikeyan@gmail.com>
2025-08-30 05:01:16 +0530

    as per video 74 changed

--- Controllers/moviesControllers.js
+++ Controllers/moviesControllers.js
@@ -1,8 +1,41 @@
 const Movie = require("./../Models/moviesModel");
 
-exports.getAllMovies = (req, res) => {};
+exports.getAllMovies = async (req, res) => {
+  try {
+    const movies = await Movie.find(req.query);
 
-exports.getAMovieById = (req, res) => {};
+    res.status(200).json({
+      status: "Success",
+      NoofMovies: movies.length,
+      data: {
+        movies: movies,
+      },
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: "Fail",
+      message: error.message,
+    });
+  }
+};
+
+exports.getAMovieById = async (req, res) => {
+  try {
+    const movie = await Movie.findById(req.params.id);
+
+    res.status(200).json({
+      status: "Success",
+      data: {
+        movie: movie,
+      },
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: "Fail",
+      message: error.message,
+    });
+  }
+};
 
 exports.createAMovie = async (req, res) => {
   try {
@@ -22,6 +55,73 @@ exports.createAMovie = async (req, res) => {
   }
 };
 
-exports.updateAMovieById = (req, res) => {};
+exports.updateAMovieById = async (req, res) => {
+  try {
+    const updtedMovie = await Movie.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
 
-exports.deleteAMovieById = (req, res) => {};
+    if (!updtedMovie) {
+      return res.status(404).json({
+        status: "Fail",
+        message: "No movie found with that ID",
+      });
+    }
+    res.status(200).json({
+      status: "Success",
+      data: {
+        updtedMovie: updtedMovie,
+      },
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: "Fail",
+      message: error.message,
+    });
+  }
+};
+
+// exports.deleteAMovieById = async (req, res) => {
+//   try {
+//     const deletedMovie = await Movie.findByIdAndDelete(req.params.id);
+//     res.status(200).json({
+//       status: "Success",
+//       message: "Deleted movie successfully",
+//       data: {
+//         deletedMovie: deletedMovie,
+//       },
+//     });
+//   } catch (error) {
+//     res.status(400).json({
+//       status: "Fail",
+//       message: error.message,
+//     });
+//   }
+// };
+
+exports.deleteAMovieById = async (req, res) => {
+  try {
+    const deletedMovie = await Movie.findByIdAndDelete(req.params.id);
+
+    if (!deletedMovie) {
+      return res.status(404).json({
+        status: "Fail",
+        message: "No movie found with that ID",
+      });
+    }
+
+    res.status(200).json({
+      status: "Success",
+      message: "Deleted movie successfully",
+      data: {
+        deletedMovie,
+      },
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: "Fail",
+      message: error.message,
+    });
+  }
+};
