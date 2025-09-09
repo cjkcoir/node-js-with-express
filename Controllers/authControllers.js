79c3ada9f3a033804575561199935e944f301428
cjkcoir <j.chellakarthikeyan@gmail.com>
2025-09-09 11:57:32 +0530

    as per video#106 JWT is created in signup function in authControllers.js

--- Controllers/authControllers.js
+++ Controllers/authControllers.js
@@ -1,10 +1,15 @@
 const User = require("./../Models/usersModel");
+const jwt = require("jsonwebtoken");
 
 exports.signup = async (req, res, next) => {
   try {
     const newUser = await User.create(req.body);
+    const token = jwt.sign({ id: newUser._id }, process.env.SECRET_STRING, {
+      expiresIn: process.env.JWT_EXPIRES_IN,
+    });
     res.status(200).json({
       status: "Success",
+      token: token,
       data: {
         createdUser: newUser,
       },
